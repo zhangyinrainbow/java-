80bcc07d2150fd2f6533260a2854c1e1d25ae143
zhangyinrainbow <825054795@qq.com>
2020-05-12 16:23:57 +0800

    类

--- Lei.java
+++ Lei.java
@@ -1,27 +1,26 @@
 public class Lei{
 	public static void main(String[] args){
-		Person xiaoming=new Person();
-		xiaoming.SetAge(18);
-		xiaoming.SetName("xiaoming");
-		String name1=xiaoming.GetName();
-		int age1=xiaoming.GetAge();
-		System.out.println(name1+"hello"+age1);
+		Person xiaomi=new Person();
+		xiaomi.setName("xiaomi");
+		xiaomi.setAge(18);
+		System.out.println(xiaomi.getAge());
+		System.out.println(xiaomi.getName());
 	}
 }
-class Person{
-		private String name;
-		private int age;
-		public void SetAge(int age){
-			this.age=age;
-		}
-		public void SetName(String name){
-			this.name=name;
-		}
-		public int GetAge(){
-			return this.age;
-		}
-		public String GetName(){
-			return this.name;
-		}
-	}
 
+class Person{
+	private int age;
+	private String name;
+	public void setName(String name){
+		this.name=name;
+	}
+	public String getName(){
+		return this.name;
+	}
+	public void setAge(int age){
+		this.age=age;
+	}
+	public int getAge(){
+		return this.age;
+	}
+}
